8b7f9ae262eaa6d5e5c546f73a72d9b902ea47e8
Atul <atulanand94@gmail.com>
2018-09-12 22:59:45 +0200

    changed blog link

--- app/components/Home/RSS/RssPane.component.js
+++ app/components/Home/RSS/RssPane.component.js
@@ -6,32 +6,46 @@ import RssEntry from './RssEntry.component';
 class RssPane extends Component {
   static propTypes = {
     feed: PropTypes.object
-  }
+  };
   getLastFewfeedEnteries = (feed = {}) => {
     const {entries = []} = feed;
     return entries.slice(0, 2); // only two recent blog posts
-  }
+  };
   parseContent = (rawHTMLContent) => {
     const span = document.createElement('span');
     span.innerHTML = rawHTMLContent;
-    return span.textContent || span.innerText;        
-  }
+    return span.textContent || span.innerText;
+  };
   render () {
     const {feed = {}} = this.props;
     return (
-      <div style={styles.container} >
+      <div style={styles.container}>
         <p style={styles.blogTitle}>RECENT BLOGS</p>
         <div style={styles.rssWrapper}>
-          {
-            this.getLastFewfeedEnteries(feed).map((entry, i) => {
-              const {link, title, isoDate, categories = []} = entry;
-              const contentSnippet = this.parseContent(entry['content:encoded']);
-              const trimmed = contentSnippet.slice(0, 200) + ' ..... '; // only 200 characters
-              return <RssEntry key={i} contentSnippet={trimmed} categories={categories} title={title} link={link} date={isoDate}/>;
-            })
-          }
-          <a href={feed.link} rel='noopener noreferrer' target='_blank' >
-            <div style={styles.blogLink}> More from Atul&lsquo;s blog  <span style={styles.nextTick}>➤</span></div>
+          {this.getLastFewfeedEnteries(feed).map((entry, i) => {
+            const {link, title, isoDate, categories = []} = entry;
+            const contentSnippet = this.parseContent(entry['content:encoded']);
+            const trimmed = contentSnippet.slice(0, 200) + ' ..... '; // only 200 characters
+            return (
+              <RssEntry
+                key={i}
+                contentSnippet={trimmed}
+                categories={categories}
+                title={title}
+                link={link}
+                date={isoDate}
+              />
+            );
+          })}
+          <a
+            href={'https://blog.atulr.com'}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <div style={styles.blogLink}>
+              {' '}
+              More from Atul&lsquo;s blog <span style={styles.nextTick}>➤</span>
+            </div>
           </a>
         </div>
       </div>
